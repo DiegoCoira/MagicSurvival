fda44ab0bd3b9010e19220b2743a879ba38b0e22
diego coira <diegocoira@gmail.com>
2023-12-02 20:30:47 +0100

    Movement and Player added

--- Scenes/Characters/Character.gd
+++ Scenes/Characters/Character.gd
@@ -3,11 +3,14 @@ class_name Character
 
 const FRICTION: float = 0.15
 
-export(int) var acceleration: int = 40
+export(int) var accerelation: int = 40
 export(int) var max_speed: int = 100
 
 var mov_direction: Vector2 = Vector2.ZERO
 var velocity: Vector2 = Vector2.ZERO
+
+onready var state_machine: Node = get_node("FiniteStateMachine")
+onready var animated_sprite: AnimatedSprite = get_node("AnimatedSprite")
 
 
 func _physics_process(_delta: float) -> void:
@@ -17,4 +20,5 @@ func _physics_process(_delta: float) -> void:
 
 func move() -> void:
 	mov_direction = mov_direction.normalized()
-	velocity += mov_direction * acceleration
+	velocity += mov_direction * accerelation
+	velocity = velocity.clamped(max_speed)
