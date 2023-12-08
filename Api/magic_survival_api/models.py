a29ea93d98b8ecdcb112b25a70a644084936658f
diego coira <diegocoira@gmail.com>
2023-12-08 14:01:07 +0100

    Models created

--- Api/magic_survival_api/models.py
+++ Api/magic_survival_api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Dialogue(models.Model):
+    name = models.CharField(max_length=30)
+    mensaje = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.personaje
+
+
+class Player(models.Model):
+    hp = models.IntegerField(max_length=1)
+
+    def __str__(self):
+        return "Player_hp"
